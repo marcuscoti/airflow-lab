36e124ed0d3455b30af67bfe9e9a7a6a88f80c36
Marcus Cotichini <marcuscoti@gmail.com>
2026-04-04 10:53:14 -0300

    States and Sales table

---
+++ airflow-container/dags/raw_sales.py
@@ -0,0 +1,126 @@
+import json
+import os
+import shutil
+from pathlib import Path
+import datetime
+import pendulum
+import pandas as pd
+from airflow import DAG
+from airflow.sdk import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.common.sql.operators.sql import SQLTableCheckOperator, SQLColumnCheckOperator
+from airflow.providers.standard.operators.bash import BashOperator, BaseOperator
+from airflow.sdk import get_current_context
+
+
+POSTGRES_CONN_ID = 'pg_nova_drive'
+
+TARGET_DIR = Path("/opt/airflow/data")
+
+WATERMARK_DIR = Path("/opt/airflow/data/watermark")
+
+TABLE_NAME = 'sales'
+
+LOAD_TYPE = 'incremental' # Full load
+
+SQL_QUERY = '''
+SELECT 
+id_vendas, 
+id_veiculos, 
+id_concessionarias, 
+id_vendedores, 
+id_clientes, 
+valor_pago, 
+data_venda, 
+data_inclusao, 
+data_atualizacao
+FROM public.vendas
+WHERE data_atualizacao > '{watermark}'
+'''
+
+WATERMARK = datetime.datetime.utcnow()
+
+TABLE_PATH = (TARGET_DIR / TABLE_NAME)
+
+FILE_PATH = f"{TARGET_DIR}/{TABLE_NAME}/batch_{WATERMARK.strftime("%Y%m%d%H%M%S")}.parquet"
+
+
+DEFAULT_ARGUMENTS = {
+    "execution_timeout": datetime.timedelta(seconds=7200),
+    "retry_delay": 300,
+    "retries": 0,
+    "priority_weight": 1,
+    "pool": "raw_bronze_pool",
+    "weight_rule": "absolute",
+}
+
+
+def _load_watermark() -> str:
+    json_file = WATERMARK_DIR / f"{TABLE_NAME}.json"
+    if json_file.exists():
+        json_content = json.loads(json_file.read_text())
+        return json_content['watermark']
+    
+    duration = datetime.timedelta(days=-365)
+
+    return str((WATERMARK + duration))
+
+
+with DAG(
+    dag_id="raw_sales",
+    description="copy states table from database",
+    schedule=None,
+    default_args=DEFAULT_ARGUMENTS,
+    start_date=pendulum.datetime(2026,1,1,tz="America/Sao_Paulo"),
+    catchup=False,
+    tags=["raw","postgre","sales","fact", "inc load"]
+) as dag:
+    
+    init_task = BashOperator(task_id="init_task", bash_command="echo Starting")
+
+
+    @task()
+    def clear_target_folder() -> None:
+        context = get_current_context()
+        ti = context['ti']
+
+        if os.path.exists(TABLE_PATH) and os.path.isdir(TABLE_PATH):
+            shutil.rmtree(TABLE_PATH)
+        else:
+            ti.xcom_push(key='Error', value="Folder does not exist or is not a directory.")
+            print("Folder does not exist or is not a directory.")
+
+
+    @task()
+    def copy_from_source_incremental() -> None:
+        TABLE_PATH.mkdir(parents=True, exist_ok=True)
+        hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+
+        watermark = _load_watermark()
+
+        print(f"Watermark: {watermark}")
+        
+        transformed_sql_query = SQL_QUERY.replace("{watermark}", watermark)
+        print(f"SQL QUERY: {transformed_sql_query}")
+
+
+        df = hook.get_df(transformed_sql_query)
+        df.to_parquet(FILE_PATH, index=False, engine='pyarrow')
+    
+
+    @task()
+    def write_watermark() -> None:
+
+        WATERMARK_DIR.mkdir(parents=True, exist_ok=True)
+
+        json_file = WATERMARK_DIR / f"{TABLE_NAME}.json"
+
+        d = {"table": str(TABLE_NAME),
+             "table_path": str(TABLE_PATH),
+             "watermark": str(WATERMARK)
+             }
+
+        json_file.write_text(json.dumps(d))
+
+
+    init_task >> clear_target_folder() >> copy_from_source_incremental() >> write_watermark()
---
+++ airflow-container/dags/raw_states.py
@@ -0,0 +1,93 @@
+import json
+import os
+import shutil
+from pathlib import Path
+import datetime
+import pendulum
+import pandas as pd
+from airflow import DAG
+from airflow.sdk import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.common.sql.operators.sql import SQLTableCheckOperator, SQLColumnCheckOperator
+from airflow.providers.standard.operators.bash import BashOperator, BaseOperator
+from airflow.sdk import get_current_context
+
+
+POSTGRES_CONN_ID = 'pg_nova_drive'
+
+TARGET_DIR = Path("/opt/airflow/data")
+
+WATERMARK_DIR = Path("/opt/airflow/data/watermark")
+
+TABLE_NAME = 'states'
+
+LOAD_TYPE = 'overwrite' # Full load
+
+SQL_QUERY = '''SELECT id_estados, estado, sigla, data_inclusao, data_atualizacao FROM public.estados;'''
+
+WATERMARK = datetime.datetime.utcnow()
+
+TABLE_PATH = (TARGET_DIR / TABLE_NAME)
+
+FILE_PATH = f"{TARGET_DIR}/{TABLE_NAME}/batch_{WATERMARK.strftime("%Y%m%d%H%M%S")}.parquet"
+
+
+
+DEFAULT_ARGUMENTS = {
+    "execution_timeout": datetime.timedelta(seconds=7200),
+    "retry_delay": 300,
+    "retries": 0,
+    "priority_weight": 1,
+    "pool": "raw_bronze_pool",
+    "weight_rule": "absolute",
+}
+
+with DAG(
+    dag_id="raw_states",
+    description="copy states table from database",
+    schedule=None,
+    default_args=DEFAULT_ARGUMENTS,
+    start_date=pendulum.datetime(2026,1,1,tz="America/Sao_Paulo"),
+    catchup=False,
+    tags=["raw","postgre","states","dimension", "full load"]
+) as dag:
+    
+    init_task = BashOperator(task_id="init_task", bash_command="echo Starting")
+
+
+    @task()
+    def clear_target_folder() -> None:
+        context = get_current_context()
+        ti = context['ti']
+
+        if os.path.exists(TABLE_PATH) and os.path.isdir(TABLE_PATH):
+            shutil.rmtree(TABLE_PATH)
+        else:
+            ti.xcom_push(key='Error', value="Folder does not exist or is not a directory.")
+            print("Folder does not exist or is not a directory.")
+
+
+    @task()
+    def copy_from_source() -> None:
+        TABLE_PATH.mkdir(parents=True, exist_ok=True)
+        hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+        df = hook.get_df(SQL_QUERY)
+        df.to_parquet(FILE_PATH, index=False, engine='pyarrow')
+
+
+
+    @task()
+    def write_watermark() -> None:
+
+        WATERMARK_DIR.mkdir(parents=True, exist_ok=True)
+
+        json_file = WATERMARK_DIR / f"{TABLE_NAME}.json"
+
+        d = {"table": str(TABLE_NAME),
+             "table_path": str(TABLE_PATH),
+             "watermark": str(WATERMARK)
+             }
+
+        json_file.write_text(json.dumps(d))
+
+    init_task >> clear_target_folder() >> copy_from_source() >> write_watermark()
