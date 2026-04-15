da5eb7d4023f023771c0e52a76bd96a96145c3d9
Marcus Cotichini <marcuscoti@gmail.com>
2026-04-15 20:27:55 -0300

    Removed testduckdb file

--- airflow-container/test_duckdb.py
+++
@@ -1,7 +0,0 @@
-import duckdb
-#duckdb.read_parquet("airflow-container\\data\\sales\\batch_20260402193137.parquet")
-print(duckdb.sql("SELECT _insert_timestamp, COUNT(*) " \
-"FROM 'airflow-container\\data\\sales\\*.parquet'" \
-"GROUP BY _insert_timestamp"
-))
-#SELECT MAX(data_atualizacao)  FROM 'airflow-container\\data\\sales\\*.parquet';
